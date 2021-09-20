50d014cbff1088184ea86a525a95cc1de4d3da52
priyamcoding <54709332+priyamcoding@users.noreply.github.com>
2021-09-20 19:45:42 +0530

    in progress

--- shop.js
+++ shop.js
@@ -1,4 +1,16 @@
-
-
+ var removeCartItemButtons = document.getElementsByClassName('btn-danger');
+console.log(removeCartItemButtons)
+for(var i=0; i<removeCartItemButtons.length; i++){
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
+    
+    })
+}
+function updateCartTotal() {
+    
+}
 
         
