b66117c7f84ded7ce62fa60c632da052a6e25439
priyamcoding <54709332+priyamcoding@users.noreply.github.com>
2021-09-26 17:17:08 +0530

    making cart dynamic

--- shop.js
+++ shop.js
@@ -1,4 +1,13 @@
 
+let itemno = 101;
+let qtypur = 01; 
+let price = 10; 
+let avail = 20; 
+
+
+
+
+
 //white shirt
 if (document.querySelector('.shop-item-button')) { 
 document.querySelector('.shop-item-button').addEventListener('click', cartClicked);
@@ -6,9 +15,18 @@ document.querySelector('.shop-item-button').addEventListener('click', cartClicke
 
 function cartClicked() 
 {
+    //  itemno_pur = itemno; 
+     itempur = qtypur;
+     price = price;
+     avail = avail; 
     alert('item is in the cart!')
+    console.log(itemno, qtypur, price, avail)
+    pur_arr=[itemno, qtypur, price, avail]
+    console.log(pur_arr)
+    p = document.createElement("h2");
+    p.innerHTML = "Your item is "+itemno;
+    document.body.appendChild(p);
 }
-
 
 //cart
 if (document.querySelector('.btn-purchase')) { 
