bb09f36d4ee4c69d8b5eb12537754545a699c289
priyamcoding <54709332+priyamcoding@users.noreply.github.com>
2021-09-26 15:51:12 +0530

    alert-working

--- shop.js
+++ shop.js
@@ -1,16 +1,21 @@
 
-document.querySelector('.btn-purchase').addEventListener('click', cartClicked);
-
-
-
-document.querySelector('.btn-purchase').addEventListener('click', purchaseClicked);
-
-
-function purchaseClicked() {
-    alert('Thank you for your purchase')
+//white shirt
+if (document.querySelector('.shop-item-button')) { 
+document.querySelector('.shop-item-button').addEventListener('click', cartClicked);
 }
 
 function cartClicked() 
 {
     alert('item is in the cart!')
 }
+
+
+//cart
+if (document.querySelector('.btn-purchase')) { 
+document.querySelector('.btn-purchase').addEventListener('click', purchaseClicked);
+}
+
+function purchaseClicked() {
+    alert('Thank you for your purchase')
+}
+
