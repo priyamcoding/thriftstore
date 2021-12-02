dc6a9c80619ed385aa29cb1496ee0a2b6caf6f02
priyamcoding <54709332+priyamcoding@users.noreply.github.com>
2021-12-02 13:52:54 +0530

    login works

--- contact.php
+++ contact.php
@@ -2,17 +2,17 @@
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 
-$con = mysqli_connect('localhost', 'priyam', 'mcmdav','shopping');
+$con = mysqli_connect('localhost', 'priyam', 'mcmdav','test');
 
 
 // get the post records
 $txtName = $_POST['txtName'];
 $txtEmail = $_POST['txtEmail'];
 $txtPhone = $_POST['txtPhone'];
-$txtMessage = $_POST['txtMessage'];
+$txtpwd = $_POST['txtpwd'];
 
 // database insert SQL code
-$sql = "INSERT INTO `tbl_contact` (`fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('$txtUserName', '$txtEmail', '$txtPhone', '$txtMessage')";
+$sql = "INSERT INTO `tbl_contact` (`fldName`, `fldEmail`, `fldPhone`, `fldtxtpwd`) VALUES ('$txtName', '$txtEmail', '$txtPhone', '$txtpwd')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);
